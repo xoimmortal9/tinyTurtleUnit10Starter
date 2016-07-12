3b8106955b15b55f2279e4ff1a61f2c23d17e1a9
Bijesse <tom@bijesse.com>
2016-07-12 10:32:34 -0400

    not working

---
+++ script.js
@@ -0,0 +1,17 @@
+TinyTurtle.apply(window);
+
+function square(){
+forward(50);
+right(90);
+forward(50);
+right(90);
+forward(50);
+right(90);
+forward(50);
+right(90);
+forward(50);
+}
+
+square();
+
+stamp();

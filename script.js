e091d616ff49a492fef2eb9636c4e5e2be7d459f
Michelle Chalco <michellechalco921@gmail.com>
2017-03-27 20:32:48 +0000

    shape funtions

--- script.js
+++ script.js
@@ -13,6 +13,67 @@ forward(50);
 }
 
 // Type your function call below
+//square();
+
+function triangle(){
+forward(50); 
+right(120);
+forward(50);
+right(120);
+forward(50);
+}
+
+//triangle();
+
+function rectangle(){
+forward(80);
+right(90);
+forward(40);
+right(90);
+forward(80);
+right(90);
+forward(40);
+}
+
+//rectangle();
+
+function pentagon(){
+    forward(50);
+    right(72);
+    forward(50);
+    right(72);
+    forward(50);
+    right(72);
+    forward(50);
+    right(72);
+    forward(50);
+}
+
+//pentagon();
+
+function hexagon(){
+    forward(40);
+    right(60);
+    forward(40);
+    right(60);
+    forward(40);
+    right(60);
+    forward(40);
+    right(60);
+    forward(40);
+    right(60);
+    forward(40);
+    
+}
+
+//hexagon();
+
+function house(){
+    triangle();
+    square();
+}
+
+house();
 
 
 stamp();
